7eda6f31cd9f6574e0366e97229b6172ee57aa7b
Michael Abreu <michael@abreuit.com>
2015-01-02 21:57:41 +0000

    Added search feature for invoices

---
+++ app/models/invoice_search.rb
@@ -0,0 +1,25 @@
+class InvoiceSearch
+  # Attributes
+  attr_reader :date_from, :date_to
+  
+  # Initialize
+  def initialize(params)
+    params ||= {}
+    @date_from = parsed_date(params[:date_from], 7.days.ago.to_date.to_s)
+    @date_to = parsed_date(params[:date_to], Date.today.to_s)
+  end
+  
+  # Helper Methods
+  def scope
+    Invoice.where('date BETWEEN ? AND ?', @date_from, @date_to)
+  end
+  
+  #Private Methods
+  private
+  
+  def parsed_date(date_string, default)
+    Date.parse(date_string)
+  rescue ArgumentError, TypeError
+    default
+  end
+end
