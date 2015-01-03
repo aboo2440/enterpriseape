f1c5835930f1cfcdd596ee380d336fbea1a41dec
Michael Abreu <michael@abreuit.com>
2015-01-03 00:31:44 +0000

    Added csv downloader for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,39 +1,58 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
-
+  # GET /companies
+  # GET /companies.json
   def index
     @companies = Company.all
-    respond_with(@companies)
+    respond_to do |format|
+      format.html
+      format.csv { render text: @companies.to_csv }
+    end
   end
 
+  # GET /companies/1
+  # GET /companies/1.json
   def show
-    respond_with(@company)
   end
 
+  # GET /companies/new
   def new
     @company = Company.new
-    respond_with(@company)
   end
 
+  # GET /companies/1/edit
   def edit
   end
 
+  # POST /companies
+  # POST /companies.json
   def create
     @company = Company.new(company_params)
-    @company.save
-    respond_with(@company)
+    
+    respond_to do |format|
+      if @company.save
+        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.json { render :show, status: :created, location: @company }
+      else
+        format.html { render :new }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
     @company.update(company_params)
-    respond_with(@company)
   end
 
+  # DELETE /companies/1
+  # DELETE /companies/1.json
   def destroy
     @company.destroy
-    respond_with(@company)
+    respond_to do |format|
+      format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
   
   def import
