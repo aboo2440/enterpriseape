0e7ee5248599099520fbdbff188e31717b6441b3
Michael Abreu <michael@abreuit.com>
2015-01-03 02:50:09 +0000

    Added Employee model/controller/view. Added Invoices to Employee model.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -41,8 +41,18 @@ class CompaniesController < ApplicationController
     end
   end
 
+  # PATCH/PUT /companies/1
+  # PATCH/PUT /companies/1.json
   def update
-    @company.update(company_params)
+    respond_to do |format|
+      if @company.update(company_params)
+        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.json { render :show, status: :ok, location: @company }
+      else
+        format.html { render :edit }
+        format.json { render json: @company.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /companies/1
@@ -61,10 +71,12 @@ class CompaniesController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_company
       @company = Company.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
       params.require(:company).permit(:name, :manager, :status, :terms)
     end
